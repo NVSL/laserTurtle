dde28bc8f5f41b00f6e478d5a1dbc757164e53ea
Chris Nelson <c4nelson@eng.ucsd.edu>
2015-09-20 22:46:37 +0000

    More comments
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/laserTurtle@22308 2cf53259-273a-af40-bda6-69d31023b8ec

--- src/SnowflakeTurtle.java
+++ src/SnowflakeTurtle.java
@@ -15,17 +15,33 @@ public class SnowflakeTurtle extends Turtle {
         super(startx, starty, modelDisplay);
     }
 
+    /**
+     * Makes an isosceles triangle. Can recursively add more triangles to each side
+     * If depth=0, it makes an upside-down 'V' shape
+     * Otherwise, it adds on more triangles
+     *
+     * @param base  Size of the base of the triangle. Does not actually get drawn
+     * @param height Height of the triangle
+     * @param depth Number of times to recurse. Higher depth means more detail
+     *
+     */
     public void triangle(int base, int height, int depth){
         double angle = Math.toDegrees(Math.atan2(2 * height, base));
-        turn(-angle);
+        turn(-angle);   //Turn to the direction of the first leg
+
+        //Length of the leg; hypotenuse of the right triangle with side lengths (base/2) and (height)
         int hypot = (int)Math.round(Math.sqrt( Math.pow(base / 2.0, 2) + Math.pow(height, 2.0) ));
         if(depth==0) {
+            //Just draw the leg
             forward(hypot);
         }else{
+            //Draw one third of the leg, a triangle, then the other third
             forward(hypot/3);
             triangle(hypot / 3, height / 3, depth - 1);
             forward(hypot/3);
         }
+
+        //Turn and do the same for the other leg
         turn(2 * angle);
         if (depth == 0) {
             forward(hypot);
@@ -34,7 +50,7 @@ public class SnowflakeTurtle extends Turtle {
             triangle(hypot / 3, height/3, depth-1);
             forward(hypot/3);
         }
-        turn(-angle);
+        turn(-angle);   //Turn back to where we started
     }
 
     /**
@@ -49,9 +65,11 @@ public class SnowflakeTurtle extends Turtle {
         SnowflakeTurtle snowflakeTurtle = new SnowflakeTurtle(960/2,150,new World(960,720));
         snowflakeTurtle.setDelay(5);        //Set a delay of 5ms between actions to make it animate
         snowflakeTurtle.setHeading(90);     //Start facing east
+
+        //Make each "side" of the snowflake
         for(int i=0;i<sides;i++){
-            snowflakeTurtle.triangle(93, 93, 2);
-            snowflakeTurtle.turn(360.0 / sides);
+            snowflakeTurtle.triangle(93, 93, 2);    //Recursively put triangles inside triangles
+            snowflakeTurtle.turn(360.0 / sides);    //Turn to the next "side"
         }
         snowflakeTurtle.writeDXF("snowflake.dxf");
     }
