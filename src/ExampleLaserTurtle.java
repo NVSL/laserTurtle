a54b2b0a163d8e3344077e663f3244672ce12e94
Chris Nelson <c4nelson@eng.ucsd.edu>
2015-11-09 18:23:12 +0000

    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/laserTurtle@22849 2cf53259-273a-af40-bda6-69d31023b8ec

--- src/ExampleLaserTurtle.java
+++ src/ExampleLaserTurtle.java
@@ -21,23 +21,40 @@ public class ExampleLaserTurtle extends Turtle {
         super(startx, starty, modelDisplay);
     }
 
+    public void drawTree(int levels, int size){
+        if(levels == 0)
+            return;
+        forward(size);
+        turn(30);
+        drawTree(levels - 1, size / 2);
+        turn(-60);
+        drawTree(levels - 1, size / 2);
+        turn(30);
+        penUp();
+        backward(size);
+        penDown();
+    }
+
     public static void main(String[] args) {
 
         //The "world" the turtle moves around in.
         //The default is 640x480 pixels (160mmx120mm) but you can make a bigger world (see the snowflake example)
-        World earth = new World();
+        World earth = new World(2000,1000);
 
         //Make a new instance of your laser turtle class and start it at (50,50)
-        ExampleLaserTurtle turtle = new ExampleLaserTurtle(50,50,earth);
+        ExampleLaserTurtle turtle = new ExampleLaserTurtle(100,500,earth);
+
+        turtle.turnToFace(2000, 500);
+        turtle.drawTree(4, 320);
 
         //Draw a triangle
-        turtle.moveTo(100, 100);
-        turtle.moveTo(200, 0);
-        turtle.moveTo(50, 50);   //Move it back to where we started
+//        turtle.moveTo(100, 100);
+//        turtle.moveTo(200, 0);
+//        turtle.moveTo(50, 50);   //Move it back to where we started
 
         //Now, convert the steps the turtle just took into something a laser cutter can understand
         //Laser cutters speak the strange language of "DXF files"
-        turtle.writeDXF("triangle.dxf");
+//        turtle.writeDXF("triangle.dxf");
     }
 }
 
